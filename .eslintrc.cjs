a2fd9221a04755b48e05457219f5ae7a7716df2e
DucPhamH <ducphamhong2@gmail.com>
2024-04-18 08:27:32 +0700

    set up

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "prettier/prettier": [
+      "warn",
+      {
+        arrowParens: "always",
+        semi: false,
+        trailingComma: "none",
+        tabWidth: 2,
+        endOfLine: "auto",
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true,
+      },
+    ],
+  },
+  ignorePatterns: [".eslintrc.cjs", "vite.config.js"],
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-yellow-300 text-4xl'>Hello</div>
+    </>
+  )
+}
+
+export default App
